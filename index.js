c64be5ce37cdc2b866aed219996e0ce79d39ad0f
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-06-02 02:52:30 -0400

    project fully functional

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const upload = require('./utils/multer.js')
 const {cloudinary} = require('./utils/cloudinary.js')
@@ -8,13 +9,27 @@ const app = express()
 app.set('view engine','ejs')
 app.use(express.json({limit:"50m"}))
 app.use(express.urlencoded({limit: "50mb", extended:false}))
-app.get('/api/upload',(req,res,next)=>{
-    res.render('index.ejs')
+
+
+
+app.get('/api/upload',async(req,res,next)=>{
+    const all_images = await cloudinary.api.resources()
+    console.log(all_images)
+    const images = await all_images.resources
+    console.log(images)
+    res.render('index.ejs', {images})
 })
 
 app.post('/api/upload',upload.single('img'),async(req,res,next)=>{
     console.log("File Details: ",req.file)
-    res.redirect('/api/upload')
+    const result = await cloudinary.uploader.upload(req.file.path)
+    console.log('result: ',result)
+    const post_details = {
+        title: req.body.title,
+        image: result.public_id
+    }
+    res.status(200).json({post_details})
+    // res.redirect('/api/upload')
 })
 
 const port = process.env.PORT || 5000

2eee96b42b6e3d1a1e3fc79ec78504483fa17a1b
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-06-02 01:32:34 -0400

    install multer

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 
-app.get('/',(req,res,next)=>{
-    res.status(200).json({"msg":"server route"})
+
+
+app.set('view engine','ejs')
+
+app.get('/api/upload',(req,res,next)=>{
+    res.render('index.ejs')
 })
+
+app.post('/api/upload',(req,res,next)=>{
+    res.redirect('/')
+})
+
 const port = process.env.PORT || 5000
 app.listen(port,()=>console.log('Server running on: ' +port))
